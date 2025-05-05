99d037a86d8b396806ab74bd50429d7907ebf6e2
kimpasignasigna <159134756+kimpasignasigna@users.noreply.github.com>
2025-05-05 23:28:01 +0800

    Add files via upload

---
+++ gallarysinulog.js
@@ -0,0 +1,140 @@
+
+const positions = ['left2', 'left1', 'center', 'right1', 'right2'];
+const carousel = document.getElementById('carousel');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+function updateCarousel(direction) {
+  const images = Array.from(carousel.children);
+  if (direction === 'next') {
+    positions.unshift(positions.pop()); // move last to front
+  } else {
+    positions.push(positions.shift()); // move first to end
+  }
+
+  images.forEach((img, i) => {
+    img.className = positions[i];
+  });
+}
+
+prevBtn.addEventListener('click', () => updateCarousel('prev'));
+nextBtn.addEventListener('click', () => updateCarousel('next'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
